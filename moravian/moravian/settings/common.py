9cba20633970473e979a53bfffdabc77b516519c
j4lib <julia.beck@lir.gu.se>
2025-08-01 15:03:11 +0200

    Add some settings for production.

--- moravian/moravian/settings/common.py
+++ moravian/moravian/settings/common.py
@@ -112,9 +112,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-        ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_build')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+MEDIA_ROOT = os.getenv('MEDIA_ROOT')
+MEDIA_URL  = os.getenv('MEDIA_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
