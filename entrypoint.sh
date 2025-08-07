6d231ba055e3000ae4116e934dc2e3ec8aca0b48
j4lib <julia.beck@lir.gu.se>
2025-08-07 13:01:49 +0200

    Add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Wait for the database to be ready
+while ! nc -z "$HOST" "$PORT"; do
+  sleep 1
+done
+
+echo "Postgres ready. Do migrations and load data..."
+python moravian/moravian/manage.py migrate
+python moravian/moravian/manage.py makemigrations pages
+python moravian/moravian/manage.py migrate pages
+
+# Create superuser if it doesn't exist
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='moravian_admin').exists():
+    User.objects.create_superuser('moravian_admin', 'admin@example.com', 'adminpass_test')
+" | python moravian/moravian/manage.py shell
+
+echo "Load sample data..."
+python moravian/moravian/manage.py loaddata sample_fixture
+python moravian/moravian/manage.py collectstatic --noinput
+
+exec python moravian/moravian/manage.py runserver 0.0.0.0:8000
