739695c4a238dbdae49d05d70c4618bcd081da79
j4lib <julia.beck@lir.gu.se>
2025-09-02 13:18:03 +0200

    Adjust fixtures to new thumbnails.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,8 @@ python moravian/moravian/manage.py migrate pages
 python moravian/moravian/manage.py makemigrations trxnviewer
 python moravian/moravian/manage.py migrate trxnviewer
 
+echo "Clean up data and load sample data... (DON'T USE IN PRODUCTION!!)"
+python moravian/moravian/manage.py flush --no-input
 # Create superuser if it doesn't exist
 echo "
 from django.contrib.auth import get_user_model
@@ -20,7 +22,6 @@ if not User.objects.filter(username='moravian_admin').exists():
     User.objects.create_superuser('moravian_admin', 'admin@example.com', 'adminpass_test')
 " | python moravian/moravian/manage.py shell
 
-echo "Load sample data..."
 python moravian/moravian/manage.py loaddata sample_fixture
 python moravian/moravian/manage.py collectstatic --noinput
 
