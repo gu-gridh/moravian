d9490c89b5a395e2c261ca07fb1a309702fab206
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2025-09-12 10:24:10 +0200

    Style fixes for pages and iiif-viewer

--- moravian/moravian/assets/js/index.js
+++ moravian/moravian/assets/js/index.js
@@ -2,11 +2,39 @@ import 'vite/modulepreload-polyfill';
 import '../css/custom.css';
 import '../css/gridh.css';
 import '../css/mobile.css';
+import '../css/ui_iiif.css';
 import OpenSeadragon from 'openseadragon';
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     OpenSeadragon({
         id: "openseadragon-viewer",
+        homeFillsViewer: false,
+                immediateRender: false,
+                visibilityRatio: 0.8,
+                minZoomImageRatio: 0.8,
+                maxZoomPixelRatio: 1,
+                homeFillsViewer: false,
+                showZoomControl: true,
+                showHomeControl: true,
+                showFullPageControl: true,
+                showNavigator: true,
+                navigatorPosition: "TOP_RIGHT",
+                navigatorAutoFade: true,
+                preserveViewport: true,
+                fullPageButton: "full-page",
+                zoomInButton: "zoom-in",
+                zoomOutButton: "zoom-out",
+                nextButton: "next-button",
+                previousButton: "prev-button",
+                showRotationControl: true,
+                rotateLeftButton: "rotate-left",
+                rotateRightButton: "rotate-right",
+                homeButton: "home",
+                gestureSettingsTouch: {
+                    pinchRotate: true
+                },
         prefixUrl: "/static/assets/images/openseadragon/",
         tileSources: {
             type: 'image',
